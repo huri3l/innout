a589ce8302c20a7c6f507c01dc8a860acaaf6325
huriel <Huriel10@users.noreply.github.com>
2020-04-20 11:36:45 -0300

    Obtendo o usuário do banco

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require_once(dirname(__FILE__, 2). '/src/models/User.php');
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
 // echo $user->getSelect();
 
-echo User::getSelect(['id' => 1], 'name, email');
-echo '<br>';
-echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
+foreach (User::get([], 'name') as $user) {
+    echo $user->name;
+    echo '<br>'; 
+}
--- src/models/Model.php
+++ src/models/Model.php
@@ -12,24 +12,42 @@ class Model {
     public function loadFromArray($arr) {
         if($arr) {
             foreach($arr as $key => $value) {
-                $this->key = $value;
+                $this->$key = $value;
             }
         }
     }
 
     public function __get($key) {
         return $this->values[$key];
+        
     }
 
-    public function __set($key, $values) {
-        $this->values[$key] = $values;
+    public function __set($key, $value) {
+        $this->values[$key] = $value;
     }
 
-    public static function getSelect($filters = [], $columns = '*') {
+    public static function get($filters = [], $columns = '*') {
+        $objects = [];
+        $result = static::getResultSetFromSelect($filters, $columns);
+        if($result) {
+            $class = get_called_class();
+            while($row = $result->fetch_assoc()) {
+                array_push($objects, new $class($row));
+            }
+        }
+        return $objects;
+    }
+
+    public static function getResultSetFromSelect($filters = [], $columns = '*') {
         $sql = "SELECT {$columns} FROM "
             .static::$tableName
             .static::getFilters($filters);
-        return $sql;
+        $result = Database::getResultFromQuery($sql);
+        if($result->num_rows === 0) {
+            return NULL;
+        } else {
+            return $result;
+        }
     }
 
     private static function getFilters($filters) {
