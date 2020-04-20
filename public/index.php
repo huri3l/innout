f75c920057e664f97396ed9a62484c838bb091f6
huriel <Huriel10@users.noreply.github.com>
2020-04-20 11:46:27 -0300

    Inserção de arquivos

--- public/index.php
+++ public/index.php
@@ -1,10 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2). '/src/config/config.php');
-require_once(dirname(__FILE__, 2). '/src/models/User.php');
-
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-print_r($user);
-echo '<br><br>';
-$user->email = 'lucas_alterado@cod3r.com.br';
-print_r($user->email);
+require_once(dirname(__FILE__, 2). '/src/views/login.php');
