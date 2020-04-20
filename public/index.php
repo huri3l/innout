b589675acaa4c30d56bed3c976f2a785c8e0b711
huriel <Huriel10@users.noreply.github.com>
2020-04-20 10:54:10 -0300

    Comando select

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ require_once(dirname(__FILE__, 2). '/src/config/config.php');
 require_once(dirname(__FILE__, 2). '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-print_r($user);
-echo '<br><br>';
-$user->email = 'lucas_alterado@cod3r.com.br';
-print_r($user->email);
+// echo $user->getSelect();
+
+echo User::getSelect(['id' => 1], 'name, email');
+echo '<br>';
+echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
