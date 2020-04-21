bc9d6ddeb228b32866e26d7d175ae20ed7b905b6
huriel <Huriel10@users.noreply.github.com>
2020-04-21 15:03:58 -0300

    Validação de Login

--- src/views/login.php
+++ src/views/login.php
@@ -1,3 +1,5 @@
+<?php $email = NULL ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,16 +22,26 @@
                 <i class="icofont-runner-alt-1 ml-2"></i>
             </div>
             <div class="card-body">
+                <?php include(TEMPLATE_PATH.'/messages.php')?>
                 <div class="form-group">
                     <label for="email">E-mail</label>
                     <input type="email" id="email" name="email"
-                        class="form-control" value="<?= $email ?>"
+                        class="form-control <?= $errors['email'] ? 'is-invalid' : '' ?>" 
+                        value="<?= $email ?>"
                         placeholder="Informe o e-mail" autofocus>
+                    <div class="invalid-feedback">
+                        <?= $errors['email'] ?>
+                    </div>
                 </div>
                 <div class="form-group">
                     <label for="password">Senha</label>
                     <input type="password" id="password" name="password"
-                        class="form-control" placeholder="Informe a senha">
+                        class="form-control <?= $errors['password'] ? 'is-invalid' : '' ?>"
+                        value="<?= $password ?>"
+                        placeholder="Informe a senha">
+                    <div class="invalid-feedback">
+                        <?= $errors['password'] ?>
+                    </div>
                 </div>
             </div>
             <div class="card-footer">
