cb4ab15ccbfd68a3674785c664e9fdd88efb918d
huriel <Huriel10@users.noreply.github.com>
2020-04-24 09:52:45 -0300

    Definição de header, footer e correção de caminhos

--- src/views/login.php
+++ src/views/login.php
@@ -1,17 +1,5 @@
 <?php $email = NULL ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/comum.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/icofont.min.css">
-    <link rel="stylesheet" href="assets/css/login.css">
-    <title>In N' Out</title>
-</head>
-<body>
     <form class="form-login" action="#" method="post">
         <div class="login-card card">
             <div class="card-header">
@@ -49,5 +37,3 @@
             </div>
         </div>
     </form>
-</body>
-</html>
