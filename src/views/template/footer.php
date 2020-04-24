2a4b512e0fb853ac1a8ca37b20e3da4600fe1f32
huriel <Huriel10@users.noreply.github.com>
2020-04-24 11:12:16 -0300

    CSS básico

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,8 +1,8 @@
 <footer class="footer">
-    <span>Desenvolvido com</span>
+    <span><h5>Desenvolvido com</span>
     <span><i class="icofont-heart text-danger mx-1"></i></span>
-    <span>por COD<span class="text-danger">3</span>R</span>
-    <br><span>Dev. Huriel</span>
+    <span>por COD<span class="text-danger">3</span>R</span></h5>
+    <span>By Huriel</span>
 </footer>
 
 </body>
