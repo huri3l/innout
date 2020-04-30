56f84758f1b2c7a5bbc41f9df8e6e31ddf6809a9
huriel <Huriel10@users.noreply.github.com>
2020-04-30 10:55:57 -0300

    Correções e ajustes para popular a base de dados

--- src/config/database.php
+++ src/config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 class Database {
+    
     public static function getConnection() {
         $envPath = realpath(dirname(__FILE__).'/../env.ini');
         $env = parse_ini_file($envPath);
