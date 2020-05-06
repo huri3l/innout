92c0272f91f912e29625627170a100d439e949d4
huriel <61247833+huri3l@users.noreply.github.com>
2020-05-06 12:09:27 -0300

    cálculo de horas mensais concluído

--- src/views/monthly_report.php
+++ src/views/monthly_report.php
@@ -1,2 +1,9 @@
-<?php
-print_r($registries);
+<main class="content">
+    <?php
+    print_r($report);
+    echo "<hr>";
+    echo $sumOfWorkedTime;
+    echo "<hr>";
+    echo $balance;
+    ?>
+</main>
