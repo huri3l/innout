9b92796eaeebe4001e72fda3adca7f3191476d4b
huriel <61247833+huri3l@users.noreply.github.com>
2020-05-09 13:22:03 -0300

    CSS de relatório gerencial

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -9,17 +9,17 @@
 
     <div class="summary-boxes">
         <div class="summary-box bg-primary">
-            <i class="icofont-users"></i>
+            <i class="icon icofont-users"></i>
             <p class="title">Quantidade de Funcionários</p>
             <h3 class="value"><?= $activeUsersCount ?></h3>
         </div>
         <div class="summary-box bg-danger">
-            <i class="icofont-patient-bed"></i>
+            <i class="icon icofont-patient-bed"></i>
             <p class="title">Faltas</p>
             <h3 class="value"><?= count($absentUsers) ?></h3>
         </div>
         <div class="summary-box bg-success">
-            <i class="icofont-sand-clock"></i>
+            <i class="icon icofont-sand-clock"></i>
             <p class="title">Horas Trabalhadas no Mês</p>
             <h3 class="value"><?= $hoursInMonth ?></h3>
         </div>
@@ -28,7 +28,7 @@
         <div class="card mt-4">
             <div class="card-header">
                 <h4 class="card-title">Faltosos do Dia</h4>
-                <p class="card-category">Relação dos funcionários que ainda não bateram o ponto</p>
+                <p class="card-category mb-0">Relação dos funcionários que ainda não bateram o ponto</p>
             </div>
             <div class="card-body">
                 <table class="table table-bordered table-striped table-hover">
